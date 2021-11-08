106be506f994fd63a68f9fb4242163c5e2a40cd6
Olexandr Bodnar <olexandr.bodnar@shiftlive.net>
2021-11-08 13:44:01 +0200

    changed modal title

--- Client/screens/ModalScreen.tsx
+++ Client/screens/ModalScreen.tsx
@@ -8,8 +8,6 @@ import { Text, View } from '../components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal test</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <GamesList path="/screens/ModalScreen.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
